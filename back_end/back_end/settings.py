221973d5a1239fafb496cdf487e2f29385db765a
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-08-06 21:53:03 +0100

    added Django REST framework and CORS

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
+    'rest_framework',
     "crypto_station"
 ]
 
@@ -127,3 +129,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
