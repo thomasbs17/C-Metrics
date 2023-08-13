719408ed3662603421a0a6a41a4326dcc8ac923f
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-08-13 12:58:17 +0100

    cleaned-up back-end code, blacked it, and optimized imports

--- crypto_station_api/views.py
+++ crypto_station_api/views.py
@@ -1,5 +1,3 @@
-import datetime
-
 import ccxt
 from GoogleNews import GoogleNews
 from django.http import JsonResponse
@@ -7,18 +5,11 @@ from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from .serializers import SimpleJSONSerializer
 
 
 class SimpleJSONView(APIView):
     parser_classes = [JSONParser]
     renderer_classes = [JSONRenderer]
-
-    def get(self, request, format=None):
-        data = {'test': 'ok'}
-        serializer = SimpleJSONSerializer(data)
-        return Response(serializer.data)
 
 
 def get_exchanges(request):
@@ -26,37 +17,37 @@ def get_exchanges(request):
     return JsonResponse(data, safe=False)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_ohlc(request):
-    exchange = request.query_params.get('exchange')
-    pair = request.query_params.get('pair')
+    exchange = request.query_params.get("exchange")
+    pair = request.query_params.get("pair")
     exchange_class = getattr(ccxt, exchange)
     exchange = exchange_class()
     ohlc_data = exchange.fetch_ohlcv(symbol=pair, timeframe="1h", limit=300)
     return JsonResponse(ohlc_data, safe=False)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_order_book(request):
-    exchange = request.query_params.get('exchange')
-    pair = request.query_params.get('pair')
+    exchange = request.query_params.get("exchange")
+    pair = request.query_params.get("pair")
     exchange_class = getattr(ccxt, exchange)
     exchange = exchange_class()
     order_book_data = exchange.fetch_order_book(symbol=pair)
     return JsonResponse(order_book_data, safe=False)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_markets(request):
-    exchange = request.query_params.get('exchange')
+    exchange = request.query_params.get("exchange")
     exchange_class = getattr(ccxt, exchange)
     exchange = exchange_class()
     return JsonResponse(exchange.load_markets(), safe=False)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_news(request):
-    pair = request.query_params.get('pair')
+    pair = request.query_params.get("pair")
     googlenews = GoogleNews()
     googlenews.get_news(pair)
     data = googlenews.results()
