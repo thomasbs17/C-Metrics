954795346a7539a202c6746022127dc32dde3eb4
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-08-13 12:44:41 +0100

    added various endpoints to back-end api (WIP)

---
+++ back_end/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for test project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from crypto_station_api.views import SimpleJSONView, get_exchanges, get_ohlc, get_order_book, get_markets, get_news
+
+# router = routers.DefaultRouter()
+# router.register(r'ohlc', SimpleJSONView.OhlcView.as_view(), 'ohlc')
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('json/', SimpleJSONView.as_view(), name='simple_json'),
+    path('exchanges/', get_exchanges, name='all_exchanges'),
+    path('markets/', get_markets, name='all_markets'),
+    path('ohlc/', get_ohlc, name='get_ohlc'),
+    path('order_book/', get_order_book, name='order_book'),
+    path('news/', get_news, name='news'),
+]
---
+++ crypto_station_api/views.py
@@ -0,0 +1,63 @@
+import datetime
+
+import ccxt
+from GoogleNews import GoogleNews
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import SimpleJSONSerializer
+
+
+class SimpleJSONView(APIView):
+    parser_classes = [JSONParser]
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request, format=None):
+        data = {'test': 'ok'}
+        serializer = SimpleJSONSerializer(data)
+        return Response(serializer.data)
+
+
+def get_exchanges(request):
+    data = ccxt.exchanges
+    return JsonResponse(data, safe=False)
+
+
+@api_view(['GET'])
+def get_ohlc(request):
+    exchange = request.query_params.get('exchange')
+    pair = request.query_params.get('pair')
+    exchange_class = getattr(ccxt, exchange)
+    exchange = exchange_class()
+    ohlc_data = exchange.fetch_ohlcv(symbol=pair, timeframe="1h", limit=300)
+    return JsonResponse(ohlc_data, safe=False)
+
+
+@api_view(['GET'])
+def get_order_book(request):
+    exchange = request.query_params.get('exchange')
+    pair = request.query_params.get('pair')
+    exchange_class = getattr(ccxt, exchange)
+    exchange = exchange_class()
+    order_book_data = exchange.fetch_order_book(symbol=pair)
+    return JsonResponse(order_book_data, safe=False)
+
+
+@api_view(['GET'])
+def get_markets(request):
+    exchange = request.query_params.get('exchange')
+    exchange_class = getattr(ccxt, exchange)
+    exchange = exchange_class()
+    return JsonResponse(exchange.load_markets(), safe=False)
+
+
+@api_view(['GET'])
+def get_news(request):
+    pair = request.query_params.get('pair')
+    googlenews = GoogleNews()
+    googlenews.get_news(pair)
+    data = googlenews.results()
+    return JsonResponse(data, safe=False)
