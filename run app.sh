46c4ae4969b401d2d07f685f04edf260fd9bc0a4
Thomas Bouamoud <thomasbs17@yahoo.fr>
2023-08-22 22:17:17 +0100

    work on websockets

--- run app.sh
+++ run app.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd 'C:\Users\Thomas Bouamoud\Code\crypto_station_front_end'
-source 'venv/Scripts/activate'
-python manage.py runserver &
+source 'venv/bin/activate'
+python3 manage.py runserver &
+python3 crypto_station_api/real-time-data.py &
 cd front_end
+npm install
 npm start
 /bin/bash
