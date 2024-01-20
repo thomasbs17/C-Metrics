c1e8ae46d2ac61d4e974f67a564d0e0ad944e5a9
Thomas Bouamoud <thomasbs17@yahoo.fr>
2024-01-20 19:50:07 +0000

    adding screening back in start up file

--- run app_linux.sh
+++ run app_linux.sh
@@ -15,7 +15,7 @@ python3 manage.py runserver &
 # RUN SERVICES
 python3 services/market_data_aggregator/real-time-data.py &
 python3 services/order_execution/main.py  &
-# python3 services/screening/main.py &
+python3 services/screening/main.py &
 
 # FRONT-END ####################################################################################
 cd front_end
