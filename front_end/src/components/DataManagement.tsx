659b9da1df10f4e793ac66c66dcd5ce98ad23609
Thomas Bouamoud <thomasbs17@yahoo.fr>
2024-01-21 19:42:27 +0000

    code formatting

--- front_end/src/components/DataManagement.tsx
+++ front_end/src/components/DataManagement.tsx
@@ -303,30 +303,30 @@ function LoadOhlcvData() {
 
 function formatOrderBook(rawOrderBook: any, isWebSocketFeed: boolean) {
   const formattedBook: OrderBookData = { bid: [], ask: [] }
-    ;['bid', 'ask'].forEach((side: string) => {
-      let cumulativeVolume = 0
-      if (isWebSocketFeed) {
-        const sortedPrices =
-          side === 'bid'
-            ? Object.keys(rawOrderBook[side]).sort(
+  ;['bid', 'ask'].forEach((side: string) => {
+    let cumulativeVolume = 0
+    if (isWebSocketFeed) {
+      const sortedPrices =
+        side === 'bid'
+          ? Object.keys(rawOrderBook[side]).sort(
               (a, b) => parseFloat(b) - parseFloat(a),
             )
-            : Object.keys(rawOrderBook[side]).sort(
+          : Object.keys(rawOrderBook[side]).sort(
               (a, b) => parseFloat(a) - parseFloat(b),
             )
-        formattedBook[side].push([0, parseFloat(sortedPrices[0])])
-        sortedPrices.forEach((price: string) => {
-          cumulativeVolume += rawOrderBook[side][price]
-          formattedBook[side].push([cumulativeVolume, parseFloat(price)])
-        })
-      } else {
-        formattedBook[side].push([0, rawOrderBook[side + 's'][0][0]])
-        rawOrderBook[side + 's'].forEach((level: [number, number, number]) => {
-          cumulativeVolume += level[1]
-          formattedBook[side].push([cumulativeVolume, level[0]])
-        })
-      }
-    })
+      formattedBook[side].push([0, parseFloat(sortedPrices[0])])
+      sortedPrices.forEach((price: string) => {
+        cumulativeVolume += rawOrderBook[side][price]
+        formattedBook[side].push([cumulativeVolume, parseFloat(price)])
+      })
+    } else {
+      formattedBook[side].push([0, rawOrderBook[side + 's'][0][0]])
+      rawOrderBook[side + 's'].forEach((level: [number, number, number]) => {
+        cumulativeVolume += level[1]
+        formattedBook[side].push([cumulativeVolume, level[0]])
+      })
+    }
+  })
   return formattedBook
 }
 
