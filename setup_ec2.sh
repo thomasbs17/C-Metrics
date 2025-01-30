e5ca7f526d3acfc3e10d158262dd2efecc4e7e4b
Thomas Bouamoud <thomasbs17@yahoo.fr>
2025-01-30 22:55:49 +0000

    updates

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -1,10 +1,12 @@
 sudo dnf install python3-devel -y
 
-
 # TA-Lib Installation:
+yum install gcc -y
+
 curl -SLO https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
 tar -xzf ta-lib-0.6.4-src.tar.gz
 cd ta-lib-0.6.4/
 ./configure --prefix=/usr
 make
 sudo make install
+cd ..
